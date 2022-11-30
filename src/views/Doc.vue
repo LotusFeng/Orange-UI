06df17bce8ae1d1f602687c6c0f40258c762a725
czf <505487452@qq.com>
2022-11-30 16:37:31 +0800

    路由切换后关闭aside，布局css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Topnav/>
+  <div class="layout">
+    <Topnav class="nav"/>
     <div class="topnav">
       <div class="logo"></div>
       <div class="menu"></div>
@@ -10,7 +10,7 @@
         <h2>组件列表</h2>
         <ol>
           <li>
-            <router-link to="/doc/swich">Swich 组件</router-link>
+            <router-link to="/doc/switch">Switch 组件</router-link>
           </li>
           <li>
             <router-link to="/doc/button">Button 组件</router-link>
@@ -23,7 +23,9 @@
           </li>
         </ol>
       </aside>
-      <main>内容</main>
+      <main>
+        <router-view/>
+      </main>
     </div>
   </div>
 </template>
@@ -42,10 +44,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
+    }
+  }
+}
+.content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
 aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
   > h2 {
     margin-bottom: 4px;
   }
@@ -54,11 +88,8 @@ aside {
       padding: 4px 0;
     }
   }
-  @media (max-width: 500px) {
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding-top: 70px;
+  main {
+    overflow: auto;
   }
 }
 </style>
