47b4d06b8bec1c64920ee3b7dcef6614e2d33f5d
czf <505487452@qq.com>
2022-12-01 11:00:26 +0800

    添加Switch 里 button 的动画

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,10 +1,18 @@
 <template>
-<button><span><span/></button>
+<button @click="toggle" :class="{checked}"><span></span></button>
 </template>
 
 <script lang="ts">
-export default {
+import {ref} from 'vue';
 
+export default {
+  setup(){
+    const checked = ref(false)
+    const toggle = ()=>{
+      checked.value = !checked.value
+    }
+    return {toggle, checked}
+  }
 }
 </script>
 
@@ -15,7 +23,7 @@ button{
   height: $h;
   width: $h*2;
   border: none;
-  background: blue;
+  background: grey;
   border-radius: $h/2;
   position: relative;
 }
@@ -27,5 +35,15 @@ span{
   width: $h2;
   background: white;
   border-radius: $h2/2;
+  transition: left 250ms;
+}
+button.checked{
+  background: blue;
+}
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+button:focus{
+  outline: none;
 }
 </style>
