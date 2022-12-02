329c5704810ac2842ee5b980331c48ff84e41da6
czf <505487452@qq.com>
2022-12-02 15:54:56 +0800

    初始化Dialog组件

---
+++ src/lib/Dialog.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header>标题</header>
+    <main>
+      <p>第一行</p>
+      <p>第二行</p>
+    </main>
+    <footer>
+      <Button>ok</Button>
+      <Button>cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue'
+export default {
+  components: {Button}
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
