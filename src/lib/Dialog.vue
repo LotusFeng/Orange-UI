c7edec195290e0b593fcfa729ad0c9b50b1d0370
czf <505487452@qq.com>
2022-12-03 11:26:42 +0800

    完成Dialog组件

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,19 +1,23 @@
 <template>
   <template v-if="visible">
-    <div class="gulu-dialog-overlay"></div>
-    <div class="gulu-dialog-wrapper">
-      <div class="gulu-dialog">
-        <header>标题 <span class="gulu-dialog-close"></span></header>
-        <main>
-          <p>第一行字</p>
-          <p>第二行字</p>
-        </main>
-        <footer>
-          <Button level="main">OK</Button>
-          <Button>Cancel</Button>
-        </footer>
+    <teleport to="body">
+      <div class="gulu-dialog-overlay" @click="onClickOverlay"></div>
+      <div class="gulu-dialog-wrapper">
+        <div class="gulu-dialog">
+          <header>
+            <slot name="title"/>
+            <span @click="close" class="gulu-dialog-close"></span>
+          </header>
+          <main>
+            <slot name="content"/>
+          </main>
+          <footer>
+            <Button @click="ok" level="main">OK</Button>
+            <Button @click="cancel">Cancel</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </teleport>
   </template>
 </template>
 
@@ -21,13 +25,48 @@
 import Button from './Button.vue';
 
 export default {
-  props:{
+  props: {
     visible: {
       type: Boolean,
       default: false
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true
+    },
+    ok: {
+      type: Function,
+    },
+    cancel: {
+      type: Function,
     }
   },
-  components: {Button}
+  components: {Button},
+  setup(props, context) {
+    const close = () => {
+      context.emit('update:visible', false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) { //props.ok && props.ok() !== false
+        close();
+      }
+    };
+    const cancel = () => {
+      props.cancel?.()
+      close();
+    };
+    return {
+      close,
+      onClickOverlay,
+      ok,
+      cancel
+    };
+  }
 };
 </script>
 
