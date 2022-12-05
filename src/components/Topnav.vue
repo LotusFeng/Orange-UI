1a546df83b24d6de6684625a000afbec36e14c2d
czf <505487452@qq.com>
2022-12-05 21:12:10 +0800

    引入svg，实现圆弧背景

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo">
+      <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-orange"></use>
+    </svg></div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -24,7 +27,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$color: #007974;
 .topnav {
+  color: $color;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -38,6 +43,10 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    > svg{
+      width: 32px;
+      height: 32px;
+    }
   }
 
   > .menu {
