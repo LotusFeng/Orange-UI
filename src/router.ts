8737c05ee9184984d9430ad1a914b9d6e3c1ab36
czf <505487452@qq.com>
2022-12-09 16:19:46 +0800

    可以部署了

--- src/router.ts
+++ src/router.ts
@@ -5,11 +5,17 @@ import SwitchDemo from "./components/SwitchPage/SwitchDemo.vue";
 import ButtonDemo from "./components/ButtonPage/ButtonDemo.vue";
 import DialogDemo from "./components/DialogPage/DialogDemo.vue";
 import TabsDemo from "./components/TabsPage/TabsDemo.vue";
+// @ts-ignore
+import intro from './markdown/intro.md'
+// @ts-ignore
+import getStarted from './markdown/get-started.md'
+// @ts-ignore
+import install from './markdown/install.md'
 
 import { h } from 'vue';
 import Markdown from './components/Markdown.vue';
 const history = createWebHashHistory();
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const md = string => h(Markdown, { content: string, key: string })
 export const router = createRouter({
   history: history,
   routes: [
@@ -19,9 +25,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", redirect: '/doc/intro'},
-        { path: "intro", component: md('intro') },
-        { path: "get-started", component: md('get-started') },
-        { path: "install", component: md('install') },
+        { path: "intro", component: md(intro) },
+        { path: "get-started", component: md(getStarted) },
+        { path: "install", component: md(install) },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
