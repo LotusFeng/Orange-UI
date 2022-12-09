d79e85459f349e8754151a5291dabb32fb943ad7
czf <505487452@qq.com>
2022-12-09 11:56:50 +0800

    添加源代码 样式

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,31 +1,31 @@
 <template>
-<div>
-  <h1>Switch 组件示例 </h1>
-  <div class="demo">
-    <h2>常规用法</h2>
-    <div class="demo-component">
-      <Switch1Demo/>
+  <div>
+    <h1>Switch 组件示例 </h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <component :is="Switch1Demo" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')" />
+      </div>
     </div>
-    <div class="demo-actions">
-      <Button>查看代码</Button>
-    </div>
-    <div class="demo-code">
-      <pre>{{Switch1Demo.__sourceCode}}</pre>
+    <div class="demo">
+      <h2>支持 disabled </h2>
+      <div class="demo-component">
+        <component :is="Switch2Demo" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')" />
+      </div>
     </div>
   </div>
-  <div class="demo">
-    <h2>支持 disabled </h2>
-    <div class="demo-component">
-      <Switch2Demo />
-    </div>
-    <div class="demo-actions">
-      <Button>查看代码</Button>
-    </div>
-    <div class="demo-code">
-      <pre>{{Switch2Demo.__sourceCode}}</pre>
-    </div>
-  </div>
-</div>
 </template>
 
 <script lang="ts">
@@ -33,15 +33,14 @@ import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
-
+import 'prismjs';
+import '../../themes/prism-okaidia.css'
+const Prism = (window as any).Prism
 import {
   ref
 } from 'vue'
 export default {
   components: {
-    Switch1Demo,
-    Switch2Demo,
-    Switch,
     Button
   },
   setup() {
@@ -50,6 +49,7 @@ export default {
       bool,
       Switch1Demo,
       Switch2Demo,
+      Prism
     }
   }
 }
